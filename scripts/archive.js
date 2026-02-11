a6f0d8fdb586478d7a6e007c3c9dadc0470d60ff
Roxas Ski <roxaskiGithub@gmail.com>
2026-02-11 11:34:54 +0000

    adjusted the amount you had to scroll to show the to top button

--- scripts/archive.js
+++ scripts/archive.js
@@ -3,7 +3,7 @@ const scrollToTopMobile = document.querySelector('.scroll-top-mobile');
 
 // displays the buttons for PC and mobile by toggling an active class when scrolling more than 5000px
 window.addEventListener('scroll', () => {
-    const displayButton = window.scrollY > 5000;
+    const displayButton = window.scrollY > 3000;
 
     scrollToTop.classList.toggle('active', displayButton);
     scrollToTopMobile.classList.toggle('active', displayButton);
