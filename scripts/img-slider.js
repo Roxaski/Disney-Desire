53e80e012a3f9af62e172ef585d273c6994b9652
Roxas Ski <roxaskiGithub@gmail.com>
2026-03-16 12:51:09 +0000

    added new functionality : you can now pause the image slider by tabbing over it, and using the arrow keys to navigate through it

--- scripts/img-slider.js
+++ scripts/img-slider.js
@@ -1,26 +1,54 @@
 const imgSlider = document.querySelector('.img-slider');
-const numberOfSlides = document.querySelectorAll('.slides');
+const slides = document.querySelectorAll('.slides');
 const slideFocus = document.querySelectorAll('.slides a');
 let activeSlide = 0;
 
 function incrementSlideImg() {
     // removes the active class from the current slide, and increments it by one
-    numberOfSlides[activeSlide].classList.remove('active');
+    slides[activeSlide].classList.remove('active');
     activeSlide++;
-    
+
     // if the number of images exceeds or is equal to the length of the slides it goes back to the first image
-    if(activeSlide >= numberOfSlides.length) {
+    if(activeSlide >= slides.length) {
         activeSlide = 0;
     };
 
-    //adds the active class to the current image that's active
-    numberOfSlides[activeSlide].classList.add('active');
+    // adds the active class to the current image
+    slides[activeSlide].classList.add('active');
+};
 
-    // keeps focus on the active element if it's focused when tabbed to
-    if(imgSlider.contains(document.activeElement)) {
-        slideFocus[activeSlide].focus();
+function decrementSlideImg() {
+    slides[activeSlide].classList.remove('active');
+    activeSlide--;
+
+    if(activeSlide < 0) {
+        activeSlide = slides.length - 1;
     };
+
+    slides[activeSlide].classList.add('active');
 };
 
 // runs the function every five seconds
-setInterval((incrementSlideImg), 5000);
+let intervalId = setInterval(incrementSlideImg, 5000);
+
+// pauses the image slider when the focus is active, resumes the image slider when focus is removed
+imgSlider.addEventListener('focusin', () => {
+    clearInterval(intervalId);
+});
+
+imgSlider.addEventListener('focusout', () => {
+    intervalId = setInterval(incrementSlideImg, 5000);
+});
+
+// adds keyboard navigation when tabbed onto the image slider, along with adding focus to the active slide
+imgSlider.addEventListener('keydown', (e) => {
+    if(e.key === 'ArrowRight') {
+        incrementSlideImg();
+        slideFocus[activeSlide].focus();
+    };
+
+    if(e.key === 'ArrowLeft') {
+        decrementSlideImg();
+        slideFocus[activeSlide].focus();
+    };
+});
