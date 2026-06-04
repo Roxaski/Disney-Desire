ae4130fd2bf68abfd490179ac238d743e3d88f1f
Roxas Ski <roxaskiGithub@gmail.com>
2026-06-04 12:08:43 +0100

    rewrote the reviews ratings system

--- scripts/movie-contents.js
+++ scripts/movie-contents.js
@@ -1,27 +1,28 @@
+// object for page theme
 const themeColors = {
     black: '#212121',
     blue: '#2056a8',
     brown: '#80604f',
     coolRed: '#a80444',
+    charcoal: '#7f7979',
     crimson: '#801c29',
     darkBlue: '#1a1c5e',
     gold: '#af814f',
     grey: '#3e3b3b',
+    lightGrey: '#cccccc',
     magenta: '#c05266ff',
     mauve: '#5b63d0ff',
     navyBlue: '#2d3d5a',
     orange: '#af5532',
     pink: '#d45b82ff',
     purple: '#57157a',
-    red: '#970c0d'
+    red: '#970c0d',
 };
 
+// global variables for page headers
 const dataAttribute = document.body.getAttribute('data-theme');
 let theme = themeColors[dataAttribute];
 const subHeaderTheme = document.querySelectorAll('.subheader');
-const starTheme = document.querySelectorAll('.star');
-const starRating = document.querySelector('.reviews:last-child');
-let userScore = 0;
 
 // checks if dark mode is active, and if so it applies the grey theme to all pages
 const darkModeTheme = window.matchMedia('(prefers-color-scheme: dark)');
@@ -30,60 +31,75 @@ if(darkModeTheme.matches) {
     theme = themeColors['grey'];
 };
 
-// styling each pages subheaders and stars with the appropriate color theme
+// styling each pages subheader with the appropriate color theme
 subHeaderTheme.forEach((subheader) => {
     subheader.style.backgroundColor = theme;
 });
 
-starTheme.forEach((stars) => {
-    stars.style.fill = theme;
-    stars.style.stroke = theme;
-});
+// global variables for star ratings
+const criticRatings = document.querySelectorAll('.critics .star')
+const starWrapper = document.querySelector('.user .star-wrapper');
+const starRatings = document.querySelectorAll('.user .star');
+const ratingsArray = Array.from(starRatings);
 
-// getting the elements from the reviews element's last child
-const star = starRating.querySelector('.star');
-const score = starRating.querySelector('#user-score');
+// getting the saved score if one exists from local storage
+let savedScore = parseInt(localStorage.getItem('userRating'));
 
-// check if a user score is already stored in local storage
-const savedScore = localStorage.getItem(document.title);
-
-// if there's a saved score we take that and convert it to a number from a string, and set the inner text of the score
-if(savedScore) {
-    userScore = parseInt(savedScore);
-    score.innerText = userScore;
-};
-
-// updates the user score and saves it to local storage
-function updateUserScore() {
-    userScore++;
-
-    if(userScore > 10) {
-        userScore = 1;
-    };
-
-    score.innerText = userScore;
-    localStorage.setItem(document.title, userScore);
-};
-
-// runs the above function everytime the star is clicked
-star.addEventListener('click', () => {
-    updateUserScore();
-});
-
-// runs the above function everytime the star is focused and user presses enter or spave keys
-star.addEventListener('keydown', (e) => {
-    if(e.key === 'Enter' || e.key === ' ') {
-        e.preventDefault();
-        updateUserScore();
+/*
+    loops through each star and checks if a saved score exists in the index, 
+    then adds an active class up to the clicked star and displays the color based on the theme
+*/
+ratingsArray.forEach((star, index) => {
+    if(index <= savedScore) {
+        star.classList.add('active');
+        star.style.fill = darkModeTheme.matches ? themeColors['charcoal'] : theme;
+    } else {
+        star.classList.remove('active');
+        star.style.fill = darkModeTheme.matches ? themeColors['grey'] : themeColors['lightGrey'];
     };
 });
 
+// looping through each critics star and checking for an active class, which then displays the color based on theme
+criticRatings.forEach((rating) => {
+    if(rating.classList.contains('active')) {
+        rating.style.fill = darkModeTheme.matches ? themeColors['charcoal'] : theme;
+    };
+});
+
+// event listener on the star ratings container, which returns whichever star was clicked on
+starWrapper.addEventListener('click', (e) => {
+    // gets the position of which star was clicked on within the array
+    const clickedStar = ratingsArray.indexOf(e.target.closest('.star'));
+
+    /*
+        since -1 dosen't exist in the array it returns early,
+        this is to stop the user from removing the star rating when clicking outside the container
+    */
+    if(clickedStar === -1) {
+        return;
+    };
+
+    // setting the saved score in local storage
+    localStorage.setItem('userRating', clickedStar);
+    
+    // adds an active class up to the clicked star in the index, and then displays the color based on the theme
+    ratingsArray.forEach((star, index) => {
+        if(index <= clickedStar) {
+            star.classList.add('active');
+            star.style.fill = darkModeTheme.matches ? themeColors['charcoal'] : theme;
+        } else {
+            star.classList.remove('active');
+            star.style.fill = darkModeTheme.matches ? themeColors['grey'] : themeColors['lightGrey'];
+        };
+    });
+});
+
+// global variables for youtube data
 const trailer = document.querySelector('.trailer');
 const button = document.querySelector('.trailer button');
 let videoID = trailer.getAttribute('data-videoId');
 let thumbnail = document.getElementById('thumbnail');
 
-// gets the youtube thumbnail and sets the video id to each designated trailer
 thumbnail.src = `https://img.youtube.com/vi/${videoID}/maxresdefault.jpg`;
 
 // plays the designated youtube trailer when clicking on the play button
